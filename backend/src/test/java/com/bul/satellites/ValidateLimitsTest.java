7d5692df3583647b199ce344a805768a8199fea3
vladt <trocenko.vlad@mail.ru>
2023-05-25 00:56:00 +0300

    add given to test

--- backend/src/test/java/com/bul/satellites/ValidateLimitsTest.java
+++ backend/src/test/java/com/bul/satellites/ValidateLimitsTest.java
@@ -1,17 +1,23 @@
 package com.bul.satellites;
 
+import com.bul.satellites.mapper.RawDataToDurationDatasets;
+import com.bul.satellites.mapper.StringToInstant;
 import com.bul.satellites.model.Given;
 import com.bul.satellites.model.Result;
+import com.bul.satellites.service.GivenLoader;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.stream.Collectors;
+import java.io.IOException;
 import java.util.stream.Stream;
 
 public class ValidateLimitsTest {
     @Test
-    void test() {
-        Given given = new Given();
+    void test() throws IOException {
+        StringToInstant stringToInstant = new StringToInstant();
+        RawDataToDurationDatasets rawDataToDurationDatasets = new RawDataToDurationDatasets(stringToInstant);
+        GivenLoader givenLoader = new GivenLoader(rawDataToDurationDatasets);
+        Given given = givenLoader.getGiven();
         Result result = new Result();
 
         long count = result.datasets.stream().flatMap(d -> d.entries.stream().flatMap(e -> Stream.of(e.start, e.end)))
