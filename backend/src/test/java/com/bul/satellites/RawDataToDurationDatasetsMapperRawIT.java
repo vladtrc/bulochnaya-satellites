a6683186394cfeb77202254ce757d7fd68e7a4f2
badma <badma.chedyrov@yahoo.com>
2023-05-25 01:23:11 +0300

    testBorders

--- backend/src/test/java/com/bul/satellites/RawDataToDurationDatasetsMapperRawIT.java
+++ backend/src/test/java/com/bul/satellites/RawDataToDurationDatasetsMapperRawIT.java
@@ -1,11 +1,14 @@
 package com.bul.satellites;
 
+import com.bul.satellites.algo.AlexeyAlgo;
 import com.bul.satellites.algo.DumbAlgo;
 import com.bul.satellites.mapper.InstantToString;
 import com.bul.satellites.mapper.RawDataToDurationDatasets;
 import com.bul.satellites.mapper.StringToInstant;
 import com.bul.satellites.model.*;
 import com.bul.satellites.service.GivenLoader;
+import com.bul.satellites.validators.LimitValidator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 @SpringBootTest
@@ -112,8 +116,7 @@ class RawDataToDurationDatasetsMapperRawIT {
                 System.out.println("An error occurred.");
                 e.printStackTrace();
             }
-        };
-   // }
+        }
 
     public void toConsole(Map<String, List<DurationDataset>> map) {
 
@@ -142,86 +145,16 @@ class RawDataToDurationDatasetsMapperRawIT {
 
     @Test
     void testGivenLoaderToTxt() throws IOException {
-        GivenLoader loader = new GivenLoader(new RawDataToDurationDatasets(new StringToInstant()),new StringToInstant());
-
+        GivenLoader loader = new GivenLoader(new RawDataToDurationDatasets(new StringToInstant()));
         toOneTxt(loader.getGiven().getAvailabilityRussia());
-
-
     }
 
     @Test
-    void testBorders(){
-        List<Interval> listInt1 = new ArrayList<>();
-        Interval int1 = Interval
-                .builder()
-                .start(Instant.parse("2072-12-11T21:59:59.860Z"))
-                .end(Instant.parse("2072-12-11T22:59:59.860Z"))
-                .build();
-        Interval int2 = Interval
-                .builder()
-                .start(Instant.parse("2072-12-11T22:39:59.860Z"))
-                .end(Instant.parse("2072-12-11T22:49:59.860Z"))
-                .build();
-        Interval int3 = Interval
-                .builder()
-                .start(Instant.parse("2072-12-10T22:59:59.860Z"))
-                .end(Instant.parse("2072-12-10T23:59:59.860Z"))
-                .build();
-        listInt1.add(int1);
-        listInt1.add(int2);
-        listInt1.add(int3);
-        SatelliteBasePair sb1 = SatelliteBasePair.builder()
-                .base("Magadan2")
-                .satellite("Kinosat1")
-                .build();
-        List<DurationDataset> dr = new ArrayList<>();
-        DurationDataset dds1 = DurationDataset.builder()
-                .entries(listInt1)
-                .satelliteBasePair(sb1)
-                .build();
-
-        List<Interval> listInt2 = new ArrayList<>();
-        Interval int12 = Interval
-                .builder()
-                .start(Instant.parse("2072-12-10T21:59:58.860Z"))
-                .end(Instant.parse("2072-12-10T22:59:59.860Z"))
-                .build();
-        Interval int22 = Interval
-                .builder()
-                .start(Instant.parse("2072-12-11T21:39:58.860Z"))
-                .end(Instant.parse("2072-12-11T21:49:59.860Z"))
-                .build();
-        Interval int32 = Interval
-                .builder()
-                .start(Instant.parse("2072-12-10T20:50:59.860Z"))
-                .end(Instant.parse("2072-12-10T20:59:59.860Z"))
-                .build();
-        listInt2.add(int12);
-        listInt2.add(int22);
-        listInt2.add(int32);
-        SatelliteBasePair sb2 = SatelliteBasePair.builder()
-                .base("Magadan")
-                .satellite("Kinosat2")
-                .build();
-
-        DurationDataset dds2 = DurationDataset.builder()
-                .entries(listInt2)
-                .satelliteBasePair(sb2)
-                .build();
-        dr.add(dds1);
-        dr.add(dds2);
-        Result.builder()
-                .datasets(dr)
-                .build();
-
-        Interval intGiven = Interval
-                .builder()
-                .start(Instant.parse("2072-12-10T20:50:59.860Z"))
-                .end(Instant.parse("2072-12-10T20:59:59.860Z"))
-                .build();
-
-
-
+    void testBorders() throws IOException {
+        GivenLoader loader = new GivenLoader(new RawDataToDurationDatasets(new StringToInstant()));
+        Result result = new AlexeyAlgo().apply(loader.getGiven());
+        LimitValidator lm= new LimitValidator(loader.getGiven());
+        lm.validate(result);
     }
 
 }
